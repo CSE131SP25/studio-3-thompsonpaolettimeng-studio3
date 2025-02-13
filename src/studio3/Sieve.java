682fa4b50d8787790177fb319a35d97aeb7b6624
samue <samue@Sam-Dell-Laptop.dhcp.wustl.edu>
2025-02-13 01:43:50 -0600

    Commit Studio 3

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -6,7 +6,21 @@ public class Sieve {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.println("Give me a number, n:");
+		int n = in.nextInt();
+		int i = 2;
+		int f = 2;
+		boolean primeNum[] = new boolean [n];
+		primeNum[0] = true;
+		primeNum[1] = true;
+		while(f <= n) {
+		for (f = 2*2;f < n;f += 2) {
+			primeNum[f] = true;
+		}
+		if((f % i) == 0) {
+		primeNum[f] = true;
+		}
 	}
-
+}
 }
